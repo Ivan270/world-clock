59c1b62425709aa4c7fa5d82ace693247d209936
Ivan Verdugo <ivan270@gmail.com>
2023-05-03 19:13:44 -0400

    project completed

--- project_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p assets/{css,img,js}
-touch index.html && touch assets/css/styles.css && touch assets/js/index.js
-
-git init
-git add .
-git commit -m "Project initialized"
-git branch -M main
-code ./
